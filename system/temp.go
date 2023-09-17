8b4ff8fcfc5796b406aff4ebe64b22269ec9c662
John Costa <john.costa@gmail.com>
2023-09-17 05:44:42 -0400

    convert string to int

--- system/temp.go
+++ system/temp.go
@@ -1,9 +1,11 @@
 package system
 
 import (
+	"log"
 	"os"
 	"runtime"
 	"strconv"
+	"strings"
 )
 
 const systemTemperatureFile = "/sys/class/thermal/thermal_zone0/temp"
@@ -20,17 +22,22 @@ func (a SystemTemperature) GetDisplayValueForSystemTemperature(thermalZone strin
 }
 
 // InCByZone returns the systems temperature in Celsius for the thermal zone specified.
-func (SystemTemperature) InCByZone(thermalZone string) (tempInC *int, err error) {
+func (SystemTemperature) InCByZone(thermalZone string) (tempInC *int64, err error) {
 	if runtime.GOOS == "linux" && runtime.GOARCH == "arm64" {
+		log.Printf("found runtime as `%s` and `%s`", runtime.GOOS, runtime.GOARCH)
 		tempData, err := os.ReadFile(systemTemperatureFile)
 		if err != nil {
 			return nil, err
 		}
 
-		*tempInC, err = strconv.Atoi(string(tempData))
+		cleanedStringTempData := strings.TrimSpace(string(tempData))
+		log.Printf("found tempData as `%s`", cleanedStringTempData)
+
+		*tempInC, err = strconv.ParseInt(cleanedStringTempData, 10, 64)
 		if err != nil {
 			return nil, err
 		}
+		return tempInC, nil
 	}
 
 	return tempInC, nil
